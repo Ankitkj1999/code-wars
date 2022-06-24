b79aa84ef095d07a41c7b4d0ad89a77cb9b9adfc
Ankitkj1999 <ankit.k.j1999@gmail.com>
2022-06-25 01:06:17 +0530

    Deodrant evaporator

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -4,5 +4,5 @@ import 'package:dart_application_1/dart_application_1.dart'
 void main(List<String> arguments) {
   // print('Hello world: ${dart_application_1.calculate()}!');
   // print(10 % 2);
-  dart_application_1.goals(2, 2, 4);
+  dart_application_1.evaporator(10, 10, 5);
 }
--- lib/dart_application_1.dart
+++ lib/dart_application_1.dart
@@ -1,4 +1,21 @@
-int goals(int laLigaGoals, int copaDelReyGoals, int championsLeagueGoals) {
-  print(laLigaGoals + copaDelReyGoals + championsLeagueGoals);
-  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+import "dart:core";
+import "dart:math";
+
+int evaporator(double content, double evapPerDay, double threshold) {
+  double exactThreshold = content * (threshold * 0.01);
+  print('Exact threshold = $exactThreshold');
+
+  int day = 0;
+
+  for (content = content; content >= exactThreshold; content) {
+    print('content = $content');
+
+    day += 1;
+    double exactEvaporationPerDay = content * (evapPerDay * 0.01);
+    print('exact evaporation = $exactEvaporationPerDay');
+    content = content - exactEvaporationPerDay;
+    print('content = $content');
+  }
+  print(day);
+  return day;
 }

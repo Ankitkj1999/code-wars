2e3547f17fdc157a260661c86096abe73f44945d
Ankitkj1999 <ankit.k.j1999@gmail.com>
2022-06-25 01:12:32 +0530

    century from the year

--- lib/dart_application_1.dart
+++ lib/dart_application_1.dart
@@ -1,21 +1,7 @@
-import "dart:core";
-import "dart:math";
-
-int evaporator(double content, double evapPerDay, double threshold) {
-  double exactThreshold = content * (threshold * 0.01);
-  print('Exact threshold = $exactThreshold');
-
-  int day = 0;
-
-  for (content = content; content >= exactThreshold; content) {
-    print('content = $content');
-
-    day += 1;
-    double exactEvaporationPerDay = content * (evapPerDay * 0.01);
-    print('exact evaporation = $exactEvaporationPerDay');
-    content = content - exactEvaporationPerDay;
-    print('content = $content');
+int century(year) {
+  if (year % 100 == 0) {
+    return year ~/ 100;
+  } else {
+    return (year ~/ 100) + 1;
   }
-  print(day);
-  return day;
 }

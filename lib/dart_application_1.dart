2aedbaed9f2afc3ad18ce92b7c9288b85c3a668e
Ankitkj1999 <ankit.k.j1999@gmail.com>
2022-06-25 02:01:20 +0530

    Nth even number

--- lib/dart_application_1.dart
+++ lib/dart_application_1.dart
@@ -1,11 +1,13 @@
-int oddCount(n) {
-  int count = 0;
-  for (int i = n - 1; i > 0; i--) {
-    if (i % 2 != 0) {
-      count++;
-      print(i);
-    }
-  }
-  print(count);
-  return count;
+int nthEven(int n) {
+//   int count = 0;
+//   int even_no = 0;
+//   for (int i = 0; count < n; i++) {
+//     if (i % 2 == 0) {
+//       even_no = i;
+//       count += 1;
+
+//     }
+//   }
+//   return even_no;
+  return ((n * 2) - 2);
 }

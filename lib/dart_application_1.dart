b1ed8ca9fc4fb9e0354704e6245d6ef63b867e6f
Ankitkj1999 <ankit.k.j1999@gmail.com>
2022-06-27 13:29:55 +0530

    Logic Drill: traffic Light

--- lib/dart_application_1.dart
+++ lib/dart_application_1.dart
@@ -1,5 +1,12 @@
-int seatsInTheater(int nCols, int nRows, int col, int row) {
-  var propleObstructed = (nCols - (col - 1)) * (nRows - row);
-  print(propleObstructed);
-  return propleObstructed;
+String updateLight(String current) {
+  if (current == 'green') {
+    print('go to yellow');
+    return 'yellow';
+  } else if (current == 'yellow') {
+    print('go to red');
+    return 'red';
+  } else {
+    print('go to green');
+    return 'green';
+  }
 }

f16d5b532c22e72bb30186adfcfce0902bb902bd
Ankitkj1999 <ankit.k.j1999@gmail.com>
2022-06-27 12:46:14 +0530

    Find the first consecutive number

--- lib/dart_application_1.dart
+++ lib/dart_application_1.dart
@@ -1,13 +1,19 @@
-int findDifference(List<int> a, List<int> b) {
-  // var oneVolume = a[0] * a[1] * a[2];
-  // var twoVolume = b[0] * b[1] * b[2];
-  // print('$oneVolume - $twoVolume');
-  // var difference = oneVolume - twoVolume;
-  // if (difference.isNegative == true) {
-  //   difference = -difference;
+int? firstNonConsecutive(List<int> arr) {
+  // for (int i in arr){
+  //   if (arr[i])
   // }
-  // print(difference);
-  // return difference;
-  var diff = (a[0] * a[1] * a[2]) - (b[0] * b[1] * b[2]);
-  return (diff.abs());
+  var index = arr.length - 1;
+
+  for (int i = 0; i <= index - 1; i++) {
+    print('values');
+    print(arr[i + 1] - arr[i]);
+    if (arr[i + 1] - arr[i] != 1) {
+      print('Non-consecutive numer is ($arr[i])');
+      print(arr[i + 1]);
+      return arr[i + 1];
+    } else {
+      print('Consecutive Numbers');
+    }
+  }
+  return null;
 }
